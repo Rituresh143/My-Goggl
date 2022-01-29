91500a71b141f8d552fe1c5a7fa4a5b0573bfae0
RITURESH KUMAR RAI <76609761+Rituresh143@users.noreply.github.com>
2022-01-29 17:42:30 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+      <div className="text-center p-10 mt-10 border-t ddark:border-gray-700 border-gray-200">
+          <h1>2021,Goggl, Inc.</h1>
+          <p>
+            Made by Rituresh Rai
+          </p>
+          <a href='https://github.com/Rituresh143'>
+        <i class="fab fa-github"></i>
+         </a>
+          
+      </div>
+  )
+};
